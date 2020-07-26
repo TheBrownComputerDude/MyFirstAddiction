ad0a7774fd107429482c7b423860811ac7b6247a
Steven <stevenlorenzochavez@gmail.com>
2020-07-26 10:38:49 -0600

    cann access videos from profile page

--- flutter_first_addiction/lib/Widgets/profilePage.dart
+++ flutter_first_addiction/lib/Widgets/profilePage.dart
@@ -1,4 +1,12 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_first_addiction/Widgets/videoPreview.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:thumbnails/thumbnails.dart';
+import 'package:video_player/video_player.dart';
+import 'package:video_thumbnail/video_thumbnail.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -8,11 +16,68 @@ class ProfilePage extends StatefulWidget {
 }
 
 class ProfilePageState extends State<ProfilePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      child: new Text("profile page"),
-    );
+
+  Future<List<FileSystemEntity>> getAllVideos() async {
+    final Directory extDir = await getApplicationDocumentsDirectory();
+    final String dirPath = '${extDir.path}/media';
+    final myDir = Directory(dirPath);
+    List<FileSystemEntity> _images;
+    _images = myDir.listSync(recursive: true, followLinks: false);
+    _images.sort((a, b) {
+      return b.path.compareTo(a.path);
+    });
+
+    return _images;
   }
 
+  Future<Uint8List> getThumbnail(String path) async {
+    return await VideoThumbnail.thumbnailData(video: path);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return new FutureBuilder(
+      future: getAllVideos(),
+      builder: (context, AsyncSnapshot<List<FileSystemEntity>> snapshot) {
+        if (!snapshot.hasData || snapshot.data.isEmpty) {
+          return Container();
+        }
+        print('${snapshot.data.length} ${snapshot.data}');
+        if (snapshot.data.length == 0) {
+          return Center(
+            child: Text('No images found.'),
+          );
+        }
+
+        return GridView.count(
+          crossAxisCount: 3,
+          children: List<Widget>.generate(
+            snapshot.data.length,
+            (index) {
+              return new Card(
+                child: new InkWell(
+                  child: new Text(snapshot.data[index].path),
+                  onTap: () async {
+                    await Navigator.push(
+                      context,
+                      new MaterialPageRoute(
+                        builder: (context) => new NewVideoPreview(snapshot.data[index].path)));
+                    setState(() {});
+                  },
+                ),
+              );
+            }),
+        );
+
+        return PageView.builder(
+          itemCount: snapshot.data.length,
+          itemBuilder: (context, index) {
+            var currentFile = snapshot.data[index].path;
+            return Container(
+              child: new Text(currentFile),
+            );
+          }
+        );
+      }
+    );
+  }
 }
